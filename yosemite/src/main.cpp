ccd871ff23c11482e3bce4a5af2efe3680325e79
11blankball <filsamare@gmail.com>
2022-12-10 15:51:12 +0700

    Setup Vulkan instance.

--- yosemite/src/main.cpp
+++ yosemite/src/main.cpp
@@ -5,8 +5,33 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
+#include <vulkan/vulkan.h>
+
+#define VK_CHECK(vkcall)					\
+		{									\
+			VkResult result_ = (vkcall);	\
+			assert(result_ == VK_SUCCESS);	\
+		}
+
+VkInstance createInstance(void)
+{
+	VkApplicationInfo appInfo = { VK_STRUCTURE_TYPE_APPLICATION_INFO };
+	appInfo.apiVersion = VK_API_VERSION_1_3;
+
+	VkInstanceCreateInfo createInfo = { VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO };
+	createInfo.pApplicationInfo = &appInfo;
+
+	VkInstance instance = 0;
+	VK_CHECK(vkCreateInstance(&createInfo, 0, &instance));
+
+	return instance;
+}
+
 int main(void)
 {
+	VkInstance instance = createInstance();
+	assert(instance);
+
 	int r = glfwInit();
 	assert(r);
 
@@ -24,6 +49,8 @@ int main(void)
 	}
 
 	glfwTerminate();
+
+	vkDestroyInstance(instance, 0);
 
 	return 0;
 }
