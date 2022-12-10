5bd92110e85e36280516b6fb3b71c180dcd1858e
11blankball <filsamare@gmail.com>
2022-12-10 15:44:15 +0700

    Basic window setup.

--- yosemite/src/main.cpp
+++ yosemite/src/main.cpp
@@ -1,8 +1,29 @@
 
+#include <assert.h>
 #include <stdio.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 
 int main(void)
 {
-	printf("Salutations, sailor!\n");
+	int r = glfwInit();
+	assert(r);
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(1024, 720, "Yosemite", 0, 0);
+	assert(window);
+
+	glfwShowWindow(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
